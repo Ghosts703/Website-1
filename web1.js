97007196a4f339dfa70e7be11691a7616cdcf1fb
Ghosts703 <fk254945+GitHub@gmail.com>
2023-10-27 18:23:41 +0500

    good work. thak gaya!

---
+++ web1.js
@@ -0,0 +1,9 @@
+document.write("Hello World. This is first website");
+
+alert("2 + 2 = 5.\nWelcome to my website\nEnjoy!");
+
+var num1, num2;
+num1 = +prompt("Enter the first number: ");
+num2 = +prompt("Enter the second number: ");
+
+alert(num1 + " + " + num2 + " = " + (num1 + num2));
